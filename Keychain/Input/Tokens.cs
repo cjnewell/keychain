c8f9ad80d4721667fe80e170324a70172f682af1
cjnewell <christopher.newell91@gmail.com>
2018-05-26 13:48:32 -0700

    Add CL token class

---
+++ Keychain/Input/Tokens.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Keychain.Input {
+
+    class Tokens {
+        private Queue<String> mTokens;
+
+        public Tokens(String xCmdLine) {
+            mTokens = new Queue<string>();
+            String[] lineItems = xCmdLine.Split(' ');
+
+            for (int ii = 0; ii < lineItems.Length; ++ii) {
+                mTokens.Enqueue(lineItems[ii]);
+            }
+        }
+
+        public String advance() {
+            return mTokens.Dequeue();
+        }
+
+        public bool isEmpty() {
+            return mTokens.Count == 0;
+        }
+
+
+    }
+}
