55c5dfb5e109b2e3443e132b0719fbecc9b41a9c
cjnewell <christopher.newell91@gmail.com>
2018-05-26 14:30:00 -0700

    Add base class for commands

---
+++ Keychain/Action/Action.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Keychain.Input;
+
+namespace Keychain.Action {
+
+    class Action {
+
+        private string mName;
+
+        public string Id {
+            get { return mName; }
+            set { mName = value; }
+        }
+
+        public Action(string xName) {
+            Id = xName;
+        }
+
+        public virtual void Run(Tokens xTokens, PasswordDictionary xDictionary) { }
+    }
+}
